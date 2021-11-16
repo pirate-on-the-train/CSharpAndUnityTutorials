1b791825a3af40518ff519698f550709658d5f9d
artem <comradegrabovsky@gmail.com>
2021-11-16 19:37:29 +0500

    Added files from 1 lesson

---
+++ Lesson 01/TypeConversions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TypeConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Явное преобразование (просто переменная получает значение другой переменной)
+            int n1 = 2021;
+            long n2 = n1;
+            Console.WriteLine(n2);       // Выведет 2021
+
+            // 
+            // Неявное преобразование
+            // 
+            // Шаблон
+            // (тип)переменная
+            // 
+            float pi = 3.1415f;
+            int a = (int)pi;
+            Console.WriteLine(a);       // Выведет 3 (обрежет дробную часть)
+
+            // Перевод строковых переменных
+            // 
+            // Шаблон
+            // (тип).Parse(переменная)
+            // 
+            string year = "2021";
+            int yearInt = int.Parse(year);
+            Console.WriteLine(year + 5);            // Выведет 20215
+            Console.WriteLine(yearInt + 5);         // Выведет 2026
+        }
+    }
+}
---
+++ Lesson 01/Variables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             * Переменная - ячейка памяти для хранения информации
+             * Шаблон:
+             * тип имя = значение;
+             * 
+             */
+
+            int i = 5;                  // Целочисленная переменная a
+            float f = 3.1415f;          // Число с плавающей точкой типа float
+            double d = 4.123454;        // Число с плавающей точкой типа double
+            char c = '_';               // Символьная переменная c
+            string name = "Nikodim";    // Строковая переменная name
+            bool b = false;             // Логическая переменная b
+
+            Console.WriteLine(i);
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+            Console.WriteLine(c);
+            Console.WriteLine(name);
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
