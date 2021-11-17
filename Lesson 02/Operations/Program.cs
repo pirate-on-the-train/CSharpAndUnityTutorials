0fb8c41abdcd0d6a9d1a73270db64284285ab249
= <=>
2021-11-17 14:58:40 +0500

    Added files from 2 lesson

---
+++ Lesson 02/Operations/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 3;
+
+            Console.WriteLine(a + b);   // Выведет 10
+            Console.WriteLine(a - b);   // Выведет 4
+            Console.WriteLine(a * b);   // Выведет 21
+            Console.WriteLine(a / b);   // Выведет 2
+            Console.WriteLine(a % b);   // Выведет 1
+            Console.WriteLine(a >> b);   // Выведет 0
+            Console.WriteLine(a << b);   // Выведет 56
+            Console.ReadKey();
+        }
+    }
+}
